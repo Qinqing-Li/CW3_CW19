1ae4be43ad3955f265e9ce2b01c9e30c0775c62a
Qinqing-Li <s2024889@ed.ac.uk>
2022-03-23 19:05:44 +0000

    ConsumerRegisterST draft

--- src/test/java/BookEventSystemTest.java
+++ src/test/java/BookEventSystemTest.java
@@ -5,7 +5,9 @@ import model.*;
 import external.*;
 import state.BookingState;
 
-
+// This system test doesn't look at implementation of methods or how class communicate with each other
+// The purpose is to feed in realistic inputs and test success case success, and fail case response
+// with desirable error message.
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,5 +27,6 @@ public class BookEventSystemTest {
         Logger.getInstance().clearLog();
         System.out.println("---");
     }
+// First, let's test the success scenario：
 
 }
--- src/test/java/ConsumerRegisterST.java
+++ src/test/java/ConsumerRegisterST.java
@@ -9,7 +9,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ConsumerRegisterST {
 
@@ -22,5 +25,69 @@ public class ConsumerRegisterST {
     void clearLogs() {
         Logger.getInstance().clearLog();
         System.out.println("---");
+    }
+
+// Create 3 realistic inputs for consumer register
+// Method name inherit from GevReport example, may need change
+// Assume boolean and int preferences are required when registering, not required later
+
+// 3 boolean values and 2 int values represent:
+// social distancing preferred (true, false)
+// air filtration preferred (true, false)
+// outdoors only preferred (true, false)
+// preferred maximum capacity
+// preferred maximum venue size
+
+    private static void register3Consumers(Controller controller) {
+        controller.runCommand(new RegisterConsumerCommand(
+                "John Biggson",
+                "jbiggson1@hotmail.co.uk",
+                "077893153480",
+                "jbiggson2",
+                true,
+                true,
+                true,
+                20,
+                20
+        ));
+        controller.runCommand(new LogoutCommand());
+        controller.runCommand(new RegisterConsumerCommand(
+                "Jane Giantsdottir",
+                "jane@inf.ed.ac.uk",
+                "04462187232",
+                "giantsRverycool",
+                true,
+                false,
+                true,
+                50,
+                50
+        ));
+        controller.runCommand(new LogoutCommand());
+        controller.runCommand(new RegisterConsumerCommand(
+                "Wednesday Kebede",
+                "i-will-kick-your@gmail.com",
+                "-",
+                "it is wednesday my dudes",
+                false,
+                false,
+                false,
+                100,
+                220
+        ));
+        controller.runCommand(new LogoutCommand());
+    }
+
+    // Now, we test the values are all stored and function properly
+    // We need to implement getResult in command
+    @Test
+    void getRegisteredConsumers(){
+        //still investigating...
+        GovernmentReport2Command cmd = new GovernmentReport2Command("Olympics Committee");
+        controller.runCommand(cmd);
+        List<Consumer> consumers = cmd.getResult();
+
+        assertEquals(2, consumers.size());
+        assertTrue(consumers.stream().anyMatch(consumer -> consumer.getName().equals("John Biggson")));
+        assertTrue(consumers.stream().anyMatch(consumer -> consumer.getName().equals("Jane Giantsdottir")));
     }
 }
