9218ccde20e5284142e978ddb68f911b94b5c320
Qinqing-Li <s2024889@ed.ac.uk>
2022-03-27 08:21:11 +0100

    BookEventST

--- src/main/java/model/GovernmentRepresentative.java
+++ src/main/java/model/GovernmentRepresentative.java
@@ -1,7 +1,7 @@
 package model;
 
 public class GovernmentRepresentative extends User {
-    public GovernmentRepresentative (String email, String password, String pymentAccountEmail){
+    public GovernmentRepresentative (String email, String password, String paymentAccountEmail){
         super(email,password,paymentAccountEmail);
     }
 }
