52716c20b842be8404d7428d055407e7b535f577
Qinqing-Li <s2024889@ed.ac.uk>
2022-04-07 07:08:31 +0100

    javadoc added

--- src/main/java/command/GovernmentReport1Command.java
+++ src/main/java/command/GovernmentReport1Command.java
@@ -7,18 +7,42 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <h1>For Government to request report(s)</h1>
+ *
+ * We implement Request Booking Records use case, where the government
+ * asks for specific information about existing bookings or consumers.
+ * The record will return two sets of dates and times.
+ *
+ * @author  CW19
+ */
+
 public class GovernmentReport1Command implements ICommand {
 
     private LocalDateTime intervalStartInclusive;
     private LocalDateTime intervalEndInclusive;
     private List<Booking> result;
 
+    /**
+     * This method get two time stamps and create an arraylist to be used later
+     * @param intervalStartInclusive First parameter, beginning time (inclusive)
+     * @param intervalEndInclusive Second parameter, ending time (inclusive)
+     * @return void
+     */
     public GovernmentReport1Command(LocalDateTime intervalStartInclusive, LocalDateTime intervalEndInclusive) {
         this.intervalEndInclusive = intervalEndInclusive;
         this.intervalStartInclusive = intervalStartInclusive;
         this.result = new ArrayList<>();
     }
 
+    /**
+     * This method first check user is government representative (only Gov rep are allowde this action)
+     * First, create an new arraylist booking. Then, for an event find all bookings and use if statement
+     * to find out all bookings made within the time period. Finally, add all such bookings into the
+     * arraylist named booking, let result arraylist = booking arraylist.
+     * @param context Only parameter
+     * @return void
+     */
     public void execute(Context context) {
 
         assert (context.getUserState().getCurrentUser() instanceof GovernmentRepresentative) :
@@ -47,7 +71,10 @@ public class GovernmentReport1Command implements ICommand {
         }
         result = bookings;
     }
-
+    /**
+     * This getter return the list of bookings
+     * @return result, an arraylist with all bookings
+     */
     public List<Booking> getResult() {
         return result;
     }
